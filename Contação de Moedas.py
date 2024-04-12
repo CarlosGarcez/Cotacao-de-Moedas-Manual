771ac220950e29e085439ec83101ddec66c22e4d
CarlosGarcez <carlosjuniorgarcez007@gmail.com>
2024-04-12 16:31:25 -0300

    novo projeto

---
+++ Contação de Moedas.py
@@ -0,0 +1,5 @@
+# Cotação de moeda
+
+real = float(input('Quantos reais você tem? R$'))
+dolar = real/4.90
+print('Com {:.2f}reais você pode comprar essa quantidade de dolar {:.2f}'.format(real,dolar))
